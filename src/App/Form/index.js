76d61fcada2eefe632aca53fcef9965870fd9786
Mariusz Smajdor <smaidorr@gmail.com>
2021-04-28 15:08:59 +0200

    Form component

---
+++ src/App/Form/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { currencies } from "./currencies";
+import "./style.css";
+
+export const Form = () => {
+  const [amount, setAmount] = useState("");
+  const [currency, setCurrency] = useState(currencies[0].short);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form className="form" onSubmit={onSubmit}>
+      <h1 className="form__header">Przelicznik walut</h1>
+      <label className="form__label">
+        <span className="form__labelText">Chcę wymienić:</span>
+        <input
+          value={amount}
+          onChange={({ target }) => setAmount(target.value)}
+          className="form__field"
+          type="number"
+          step="0.01"
+          placeholder="kwota w pln"
+        />
+      </label>
+      <label className="form__label">
+        <span className="form__labelText">Chcę otrzymać:</span>
+        <select
+          className="form__field"
+          value={currency}
+          onChange={({ target }) => setCurrency(target.value)}
+        >
+          {currencies.map((currency) => (
+            <option key={currency.short}>{currency.name}</option>
+          ))}
+        </select>
+      </label>
+      <button className="form__submit">Przelicz</button>
+      <p className="form__result"></p>
+    </form>
+  );
+};
