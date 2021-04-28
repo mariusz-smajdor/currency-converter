cf7407289c88d9677b515f8e31b067989670c382
Mariusz Smajdor <smaidorr@gmail.com>
2021-04-28 15:00:58 +0200

    Currencies object

---
+++ src/App/Form/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+  {
+    name: "Funt brytyjski",
+    short: "GBP",
+    rate: 5.2396,
+  },
+  {
+    name: "Euro",
+    short: "EUR",
+    rate: 4.5231,
+  },
+  {
+    name: "Dolar amerykański",
+    short: "USD",
+    rate: 3.7572,
+  },
+];
