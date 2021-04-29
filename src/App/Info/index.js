a5ea4d1a3d1e76becb5bfafbabaeb5a7a3f071be
Mariusz Smajdor <smaidorr@gmail.com>
2021-04-29 13:32:53 +0200

    Corrected class to className

--- src/App/Info/index.js
+++ src/App/Info/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./style.css";
 
 export const Info = () => (
-  <section class="info">
-    <p class="info__paragraph">
+  <section className="info">
+    <p className="info__paragraph">
       Kursy pochodzą ze strony nbp.pl z tabeli nr 040/A/NBP/2021 z dnia
       2021-03-01
     </p>
