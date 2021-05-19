e5d5ea90671904516fb04792f4a4e476a17af965
Mariusz Smajdor <smaidorr@gmail.com>
2021-05-19 13:57:33 +0200

    moved formatDate function outside Timer component

--- src/App/Form/Timer/index.js
+++ src/App/Form/Timer/index.js
@@ -2,18 +2,18 @@ import React from "react";
 import { Clock } from "./styled";
 import { useCurrentDate } from "./useCurrentDate";
 
+const formatDate = (date) =>
+  date.toLocaleString(undefined, {
+    weekday: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    day: "numeric",
+    month: "long",
+  });
+
 export const Timer = () => {
   const date = useCurrentDate();
-
-  const formatDate = (date) =>
-    date.toLocaleString(undefined, {
-      weekday: "long",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      day: "numeric",
-      month: "long",
-    });
 
   return <Clock>Dzisiaj jest {formatDate(date)}</Clock>;
 };
